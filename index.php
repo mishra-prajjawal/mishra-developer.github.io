b582da6f511e16b7ddfd4916ccc201bcf8dbbe33
mishra-developer <94694798+mishra-developer@users.noreply.github.com>
2022-02-15 15:35:39 +0530

    index.php
    this is my sites first page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+  echo time(); 
+  echo date("d-m-y");echo "Thanks to visit by Prajjawal will soon be adding his repos to github"; 
+  u=0; 
+  while(u<10):
+    u++; 
+    if(u==1):
+      echo "Loading..."
+    elseif(u==2):
+      echo "Substance instance loaded [ray_id]=> (".hash("sha384", time()).")";
+    elseif(u==3):
+      echo "Loading... k instances loading... cookie loader set";
+    elseif(u==4):
+      echo "Loading complete , Welcome to K substances mix gen engine [this_snippet]=>[prajjawal_mishra]";
+    elseif(u==5):
+      echo "System crumbled";
+    else:
+      echo "Alright, Something failed :( !!!, Message sent to mishra-developer , dev-mskm";
+      echo"/lookslike engine rep failed/ "; 
+    
+
+?>
